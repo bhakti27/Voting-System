eb91c13b212549b05b853b1ffeb60835e2fad585
bshah7 <Bhakti_Shah@intuit.com>
2020-07-16 09:31:19 -0700

    adding store set up and store

---
+++ vote-system/src/reducers/votingSystemReducers.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+
+
+export const votingSystemReducer = combineReducers
+({
+    //TODO  : add reducers to be combined here
+});
---
+++ vote-system/src/stores/votingSystemStore.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import { votingSystemReducer } from '../reducers/votingSystemReducers';
+
+export const votingSystemStore = createStore(votingSystemReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
