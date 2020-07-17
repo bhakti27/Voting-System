e585eda44c7ce91263759da9628280d0c91b90be
ccloes <ccloes@gmail.com>
2020-07-16 17:39:07 -0700

    Updated to include additional formatting

--- vote-system/src/components/FormControls.js
+++ vote-system/src/components/FormControls.js
@@ -4,9 +4,7 @@ export const StringFormControl = (props) => {
 
   return (
     <div id='input-field'>
-     <div class="column"></div>
-      <label htmlFor={props.name + '-input'}>{props.caption}</label>
-      <div class="column"></div>
+      <label id="input-field-text" htmlFor={props.name + '-input'}>{props.caption}</label>
       <input type="text" id={props.name + '-input'} name={props.name}
         value={props.value} onChange={props.onChange} />
     </div>
